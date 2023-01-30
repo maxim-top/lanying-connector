04ee4f629d5f5158ce5d42f65342999323bb4cd5
ZouJinHai <jinhai@bmxlabs.com>
2023-01-30 16:35:04 +0800

    Support use openai playground preset code directly

--- services/openai_service.py
+++ services/openai_service.py
@@ -4,6 +4,12 @@ def handle_chat_message(content, config):
     openai.api_key = config['openai_api_key']
     preset = config['preset']
     prompt = preset['prompt']
-    preset['prompt'] = prompt.replace('{{LANYING_MESSAGE_CONTENT}}', content)
+    if prompt.find('{{LANYING_MESSAGE_CONTENT}}') > 0:
+        preset['prompt'] = prompt.replace('{{LANYING_MESSAGE_CONTENT}}', content)
+    else:
+        if len(preset['stop']) == 2:
+            preset['prompt'] = prompt + preset['stop'][0] + content + "\n" + preset['stop'][1]
+        else:
+            preset['prompt'] = prompt + "Human:" + content + "\n" + "AI:"
     response = openai.Completion.create(**preset)
     return response.choices[0].text.strip()
